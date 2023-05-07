cf648f0be742425473fbdf7228325d847d14dcf8
snd <viper.ghost@bk.ru>
2023-05-07 21:48:17 +0500

    Add User and UserManager models

--- CORE/settings.py
+++ CORE/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_framework.atuhetoken',
+    'rest_framework.authtoken',
     'api.apps.ApiConfig',
 ]
 
@@ -127,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'api.User'
